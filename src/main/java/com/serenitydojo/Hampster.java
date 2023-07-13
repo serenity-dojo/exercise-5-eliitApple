e2498ef0841ad9745ecff618ed732bd235805fa7
eliitApple <eliit.apple@gmail.com>
2023-07-13 11:34:36 +0200

    Add tests and fix Hampster class

--- src/main/java/com/serenitydojo/Hampster.java
+++ src/main/java/com/serenitydojo/Hampster.java
@@ -3,13 +3,13 @@ package com.serenitydojo;
 public class Hampster extends Pet {
     private String favoriteGame;
 
-    public Hampster(String name, int age) {
+    public Hampster(String name, int age, String wheel) {
         super(name, age);
     }
 
     @Override
     public String play() {
-        return "plays with wheel";
+        return "runs in wheel";
     }
 
 
