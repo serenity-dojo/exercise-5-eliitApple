84d64332067d1cddf9e7c6ffa4707eb8d54f84ec
eliitApple <eliit.apple@gmail.com>
2023-07-13 11:12:00 +0200

    Add Hampster class

---
+++ src/main/java/com/serenitydojo/Hampster.java
@@ -0,0 +1,9 @@
+package com.serenitydojo;
+
+public class Hampster extends Pet {
+    private String favoriteGame;
+
+    public Hampster(String name, int age) {
+        super(name, age);
+    }
+}
